55781746bd719591931162c22c0ffb46fb1d653e
Sujan Koirala <077bct087.sujan@pcamupus.edu.np>
2022-05-29 18:22:06 +0545

    Ball random bounce success

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from turtle import width
 import pygame
 from pygame.locals import *
 WIDTH, HEIGHT = 800, 600
@@ -10,15 +11,19 @@ def createWindow(w,h, caption):
     pygame.display.set_caption(caption)
 createWindow(WIDTH, HEIGHT, CAPTION)
 
+ball_speedX = 10
+ball_speedY = 5
+
 running = True
 paddlex, paddley  = WIDTH/2-20, HEIGHT -70
+ballx, bally = 400, 300
 class Paddle:
     width, height = 140, 15
     def __init__(self, paddlex, paddley):
         self.x = paddlex
         self.y = paddley
         
-    def createRectangle(self):
+    def createPaddle(self):
         #   Get mouse postion as a tuple
         mx, my = pygame.mouse.get_pos()
         #   Set 'y' to original value to avoid moving up and down
@@ -27,15 +32,37 @@ class Paddle:
         self.y = paddley
         pygame.draw.rect(win, white, (self.x, self.y, self.width, self.height) )
         
-    
+
 class Ball:
-    def __init__(self, ballx, bally):
-        self.ballx = ballx
-        self.bally = bally
-
-
+    height = 10
+    width = 10
+    def __init__(self, x,y , velocityX, velocityY):
+        self.x = x
+        self.y = y
+        self.velx= velocityX
+        self.vely = velocityY
+    def createBall(self):
+        pygame.draw.rect(win, white, (self.x, self.y, self.width, self.height) )
+        self.ball_bouncing_mechanism()
+        self.moveBall()
+        
+    def moveBall(self):
+        self.x += self.velx
+        self.y += self.vely
+    
+    def ball_bouncing_mechanism(self):
+        if self.x <= 0:
+            self.velx *= -1
+        if self.x >= WIDTH - 10:
+            self.velx *= -1
+        if self.y <=5 :
+            self.vely *= -1
+        if self.y >= HEIGHT - 10:
+            self.vely *= -1
 #   Creating paddle object
 paddle = Paddle(paddlex, paddley)
+#   Creating ball
+ball = Ball(ballx, bally, ball_speedX, ball_speedY)
 
 while (running):
     win.fill((0, 0,0))
@@ -46,5 +73,6 @@ while (running):
         if event.type == pygame.KEYDOWN:
             if event.key == K_q:
                 running = False
-    paddle.createRectangle()
+    paddle.createPaddle()
+    ball.createBall()
     pygame.display.update()
